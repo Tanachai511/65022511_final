a98f7c0ba9e48f926bb015513ba0b3f574d4c032
Tanachai <65022511@up.ac.th>
2023-02-22 15:09:56 +0700

    push

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const table = document.getElementById('table')
-
-function text_change(val){
-    const TB = document.getElementById('Name').value;
-    console.log(TB);
-    table.innerText =TB;
-}
-
 function selectedMenu(){
     const selected = document.getElementById('menu')
     const selectedTxt = document.getElementById('selected');
